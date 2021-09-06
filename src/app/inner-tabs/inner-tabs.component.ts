cf76b3f355e0c34573f5172bcd4b718b30a8815b
sahare-11 <89445842+sahare-11@users.noreply.github.com>
2021-09-06 14:19:10 +0530

    final-commit

--- src/app/inner-tabs/inner-tabs.component.ts
+++ src/app/inner-tabs/inner-tabs.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-inner-tabs',
   templateUrl: './inner-tabs.component.html',
-  styleUrls: ['./inner-tabs.component.css']
+  styleUrls: ['./inner-tabs.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class InnerTabsComponent implements OnInit {
 
